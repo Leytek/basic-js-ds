fc1c5b376f49648ada4b99790bbebe9bdbba7853
Leytek <leytek@yandex.ru>
2022-05-02 21:13:06 +0300

    feat: implement Queue

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError, ListNode} = require('../extensions/index.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -14,20 +14,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
+  #head;
+  #end;
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.#head;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    if(!this.#head) {
+      this.#head = new ListNode(value);
+      this.#end = this.#head;
+    }
+    else {
+      this.#end.next = new ListNode(value);
+      this.#end = this.#end.next;
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const value = this.#head.value;
+    this.#head = this.#head.next;
+    return value;
   }
 }
 
